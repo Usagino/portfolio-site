ba05e9d88c378e58413dc5a1c5373f92f5028d87
Usagino <yuki.itoh@front-artgraph.com>
2019-05-23 19:22:03 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,40 @@
 section
   cursorPointer
   .first
+    .first__wrap
+      .first__wrap__logo
+        lottie(
+          :options="lottieOptions"
+          v-on:animCreated="handleAnimation")
     h1 test
 </template>
 
 <script>
 // components
 import cursorPointer from '@/components/cursorPointer'
+import Lottie from '@/components/Lottie'
+import * as animationData from "~/assets/json/my_logo.json";
 // library
 import products from '@/assets/json/product.json'
 
 
 export default {
   components: {
-    cursorPointer
+    cursorPointer,
+    Lottie
+  },
+  data() {
+    return {
+      lottieOptions: {
+        animationData: animationData
+      },
+      animationSpeed: 1,
+    }
+  },
+  methods: {
+    handleAnimation: function(anim) {
+      this.anim = anim;
+    },
   },
 }
 </script>
