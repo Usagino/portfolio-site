3dddbd90f6b14eddcf18d19aa3da69e503a78a7b
yuki <yuki.itoh@front-artgraph.com>
2020-02-04 21:13:24 +0900

    error.vue add Vue Footer

--- layouts/error.vue
+++ layouts/error.vue
@@ -6,17 +6,13 @@
         h1.container__not-found--text 404
       <h1 v-else>エラーが発生しました</h1>
       <h2><nuxt-link to="/">Back to Home</nuxt-link></h2>
+    VueFooter
 </template>
 
 <script>
-import sideMenu from '@/components/sideMenu'
-import VueFooter from '@/components/VueFooter'
 export default {
   props: ['error'],
   layout: 'blog', // エラーページ用のカスタムレイアウトを指定できます,
-  components: {
-    sideMenu,
-  }
 }
 </script>
 <style lang="scss" scoped>
